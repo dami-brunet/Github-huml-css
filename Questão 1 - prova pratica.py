02268b8bd05b3e341402823268437451f6689e6b
Damira <140218971+dami-brunet@users.noreply.github.com>
2023-08-29 16:13:41 -0300

    Add files via upload

---
+++ Questão 1 - prova pratica.py
@@ -0,0 +1,7 @@
+x = float(input("Digite a sua idade"))
+if x <= 18:
+    print("Você é menor de idade")
+elif x >= 19 and x <= 65:
+    print("Você é adulto")
+else:
+    print("Você é idoso")
---
+++ Questão 2 - prova pratica.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um numero:"))
+if a > 0:
+    print("Positivo")
+elif a < 0:
+    print("Negativo")
+elif a == 0:
+    print("Zero")
